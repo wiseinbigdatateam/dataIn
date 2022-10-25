d461b01d775d799642cf1c34eb97c36fa19aa19d
gimdaehyeon <kdhdh560@naver.com>
2022-10-25 15:27:22 +0900

    add ServiceApllication

---
+++ multi-faceted-evaluation/src/main/java/com/wiseinc/dataIn/MultiFacetedServiceApplication.java
@@ -0,0 +1,4 @@
+package com.wiseinc.dataIn;
+
+public class MultiFacetedServiceApplication {
+}
---
+++ satisfaction/src/main/java/com/wiseinc/dataIn/SatisfactionServiceApplication.java
@@ -0,0 +1,13 @@
+package com.wiseinc.dataIn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SatisfactionServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SatisfactionServiceApplication.class, args);
+    }
+
+}
