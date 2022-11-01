66bae01545b4cd912c3ab95b98eecd344da83704
gimdaehyeon <kdhdh560@naver.com>
2022-11-01 14:13:11 +0900

    build.gradle 변경

--- ahp/src/main/java/com/wiseinc/dataIn/AhpServiceApplication.java
+++ ahp/src/main/java/com/wiseinc/dataIn/AhpServiceApplication.java
@@ -1,4 +1,11 @@
 package com.wiseinc.dataIn;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
 public class AhpServiceApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(AhpServiceApplication.class, args);
+    }
 }
--- multi-faceted-evaluation/src/main/java/com/wiseinc/dataIn/MultiFacetedServiceApplication.java
+++ multi-faceted-evaluation/src/main/java/com/wiseinc/dataIn/MultiFacetedServiceApplication.java
@@ -1,4 +1,12 @@
 package com.wiseinc.dataIn;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
 public class MultiFacetedServiceApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(MultiFacetedServiceApplication.class, args);
+    }
 }
+
