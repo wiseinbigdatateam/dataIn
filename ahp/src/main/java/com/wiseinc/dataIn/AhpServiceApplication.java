7dcba0073ee8b0425e6769a14f6b968df233dfc3
gimdaehyeon <kdhdh560@naver.com>
2022-11-02 14:05:27 +0900

    build.gradle 내용 추가

--- ahp/src/main/java/com/wiseinc/dataIn/AhpServiceApplication.java
+++ ahp/src/main/java/com/wiseinc/dataIn/AhpServiceApplication.java
@@ -2,7 +2,9 @@ package com.wiseinc.dataIn;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
+@EnableEurekaClient
 @SpringBootApplication
 public class AhpServiceApplication {
     public static void main(String[] args) {
